f1b09c25cc98099fc6f8cc1278f5205fd70e65db
Vincent Wiederhold <vincent.taliesin@gmail.com>
2025-10-21 19:49:03 +0200

    leg i vue

---
+++ frontendprojekt/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import NyHeader from './components/NyHeader.vue';
+</script>
+
+<template>
+  <NyHeader/>
+</template>
+
+<style scoped>
+</style>
---
+++ frontendprojekt/src/components/NyHeader.vue
@@ -0,0 +1,29 @@
+<script setup>
+</script>
+
+<template>
+    <div>
+        <ul>
+            <li>
+                Forside
+            </li>
+            <li>
+                produkter
+            </li>
+            <li>
+                Om os
+            </li>
+        </ul>
+    </div>
+</template>
+<style scoped>
+
+ul{
+    display: flex;
+    flex-direction: row;
+}
+li{
+    list-style: none;
+    padding: 20px;
+}
+</style>
