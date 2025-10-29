9ae3ea18d443ffa09a673f1d9ef3af281719d760
Vincent Wiederhold <vincent.taliesin@gmail.com>
2025-10-29 11:29:12 +0100

    update

--- frontendprojekt/src/components/OpskriftSøger.vue
+++ frontendprojekt/src/components/OpskriftSøger.vue
@@ -12,10 +12,20 @@ favoritOpskrift.value = JSON.parse(localStorage.getItem('farvoritter')) || [];
 const findOpskifter = () => { 
     fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${søgeInput.value}&apiKey=d95e6064b25548ff938cc47738f1a41a`)
     .then((response) => response.jason())
-    .then((data))
+    .then((data) => { 
+        opskriftOutput.value = data.results;
+        localStorage.setItem('søgeresultater', JSON.stringify(opskriftOutput.value));
+    })
+    .catch((error) = console.error(error));
 }
 
+const rydSøgningsliste = () => {
+    søgeInput.value = '';
+    opskriftOutput.value = [];
+    localStorage.removeItem('søgeresultater')
+}
 </script>
+<h2>Søg på opskrifter</h2>
 
 <template>
 
